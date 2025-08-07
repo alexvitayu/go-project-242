59c9895e6c1ef46488a4173bd6d19e91be5f8275
Aliaksei Yuryeu <alexvitayu1212@gmail.com>
2025-08-07 13:35:44 +0300

    Add interface of command line

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
 	fmt.Println("Hello from Hexlet!")
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "print size of a file or directory;",
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+
 }
--- path_size.go
+++ path_size.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func Help() {
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "print size of a file or directory;",
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
